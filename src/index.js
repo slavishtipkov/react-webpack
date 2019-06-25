478c462d8280f87ee590abfe8af8f99ed863499b
Slavi Shtipkov <slavishtipkov1993@gmail.com>
2019-06-25 19:46:59 +0300

    Add 'source-map' and obvious error in 'sum.js'

--- src/index.js
+++ src/index.js
@@ -2,7 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 import App from "./App";
+import sum from "./sum.js";
 
+console.log(sum(2, 5));
 const title = "React with Webpack and Babel!";
 
 ReactDOM.render(<App title={title} />, document.getElementById("app"));
--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,7 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   entry: "./src/index.js",
+  devtool: "source-map",
   module: {
     rules: [
       {
