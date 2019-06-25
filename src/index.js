aff66be17175070902da4d10fc54d6d17a154549
Slavi Shtipkov <slavishtipkov1993@gmail.com>
2019-06-25 17:53:59 +0300

    Add simple React app, babel config, hot reload and more

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
-console.log("Webpacksss dev server.. ");
+import React from "react";
+import ReactDOM from "react-dom";
+
+import App from "./App";
+
+const title = "React with Webpack and Babel";
+
+ReactDOM.render(<App title={title} />, document.getElementById("app"));
+
+module.hot.accept();
